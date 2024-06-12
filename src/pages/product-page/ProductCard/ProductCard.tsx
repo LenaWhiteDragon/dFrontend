d5fddc40443547b7f48e4569a3f3e2bcb6749f9b
Dima Danilov <nervogryz@gmail.com>
2024-06-12 04:43:44 +0300

    add image click link

--- src/pages/product-page/ProductCard/ProductCard.tsx
+++ src/pages/product-page/ProductCard/ProductCard.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import styles from "./ProductCard.module.scss";
 import imagePlaceholder from "../../../assets/images/image_placeholder.png";
 
@@ -17,10 +17,18 @@ export function ProductCard(props: Props) {
 
   return (
     <div className={styles.cardProduct}>
-      <img src={props.productPhoto || imagePlaceholder} alt="Product" />
+      <Link
+        to={"/orderProduct/product/" + props.productId}
+        className={styles.cardContainer}
+      >
+        <img
+          src={props.productPhoto || imagePlaceholder}
+          alt="Product"
+          className={styles.cardImg}
+        />
+      </Link>
       <div className={styles.cardInfo}>
         <p>{props.productName}</p>
-        {/* <p>{props.doctorSpecialty}</p> */}
         <button onClick={goToProduct}>Перейти к товару</button>
       </div>
     </div>
